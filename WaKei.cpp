5810310f689d06748064a5a2f4c90985e3746b68
Landon Ellis <landon.ellis2@gmail.com>
2020-03-19 22:28:56 -0400

    Async Updating

--- WaKei.cpp
+++ WaKei.cpp
@@ -1,16 +1,69 @@
 #include "WaKei.h"
 #include <Windows.h>
+#include <thread>
 
 static const int MAX_KEYS = VK_OEM_CLEAR + 1;
 /* 0-Up,1-Released,2-Down,3-Pressed */
 static byte keys[MAX_KEYS] = { 0 };
-static const int SIG_BIT = 0x8000;
+const int WaKei::SIG_BIT = 0x8000;
+static std::thread updateThread;
+bool WaKei::isAsync = false;
+long WaKei::SLEEP_TIME = 20;
+void(*WaKei::keyCallback)(int, bool) = nullptr;
 static enum {
 	UP = 0,
 	RELEASED,
 	DOWN,
 	PRESSED
 };
+
+static void WaKeiUpdateLoop() {
+
+	while (WaKei::isAsync) {
+
+		WaKei::update();
+		Sleep(WaKei::SLEEP_TIME);
+
+	}
+
+}
+
+void WaKei::setUpdateFrequency(long time) {
+
+	SLEEP_TIME = time;
+
+}
+
+void WaKei::setKeyCallback(void(*f)(int key, bool pressed)) {
+
+	keyCallback = f;
+
+}
+
+void WaKei::start() {
+
+	if (isAsync) {
+		return;
+	}
+
+	isAsync = true;
+	updateThread = std::thread(WaKeiUpdateLoop);
+
+}
+
+void WaKei::stop() {
+
+	if (!isAsync) {
+		return;
+	}
+
+	isAsync = false;
+	if (std::this_thread::get_id() != updateThread.get_id()
+		&& !updateThread.joinable()) {
+		updateThread.join();
+	}
+
+}
 
 void WaKei::update() {
 
@@ -20,6 +73,9 @@ void WaKei::update() {
 		}
 		else {
 			keys[i] = (!keys[i] ? UP : RELEASED);
+		}
+		if (keyCallback && (keys[i] == PRESSED || keys[i] == RELEASED)) {
+			keyCallback(i, keys[i] == PRESSED);
 		}
 	}
 
--- WaKei.h
+++ WaKei.h
@@ -6,15 +6,31 @@
 class WaKei {
 public:
 
+	/* Start async update thread */
+	static void start();
+	/* End async update thread */
+	static void stop();
 	/* Update current input data */
 	static void update();
+	/* Is the key down since last update */
 	static bool isKeyDown(int);
+	/* Was key pressed most recent update */
 	static bool isKeyPressed(int);
+	/* Was key released most recent update */
 	static bool isKeyReleased(int);
-
+	/* Callback for key press and release */
+	static void setKeyCallback(void(*f)(int key, bool pressed));
+	/* Set async update loop sleep time */
+	static void setUpdateFrequency(long);
 
 private:
 
-	
+	friend void WaKeiUpdateLoop();
+
+	static const int MAX_KEYS;
+	static const int SIG_BIT;
+	static bool isAsync;
+	static long SLEEP_TIME;
+	static void(*keyCallback)(int, bool);
 
 };
